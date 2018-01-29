a75cb7c985ce8ecea6d4749a48878f3ced32216c
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 14:00:34 +0530

    updated commit - confirm subscription

--- Hiru_M3_SNS/lambda.js
+++ Hiru_M3_SNS/lambda.js
@@ -3,48 +3,18 @@ const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
 	console.log('This is the log to know that trigger has started', event);
-	sns.getTopicAttributes({
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
+	sns.confirmSubscription({
+		Token: '2336412f37fb687f5d51e6e241da92fd74469bcf40ac3553960a9221f6189934edbd390790357d4675be3cb01f931b6a2eebc6d918f52881f320f7586928045eed26570762662deea161da12ebcd4c77dcfd7cc7bdc6bd0abc50cb05bcd5601036855fb316e47d5cf374e21780fa14d3361a3c9e97b5939d74749c18ddab923f',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru',
+		AuthenticateOnUnsubscribe: 'false'
 	}).promise()
 		.then(data => {
-			console.log('success getTopicAttributes', data);
+			console.log('success confirmSubscription ',data);
 		})
 		.catch(err => {
-			console.log('error getTopicAttributes ', err);
-		});
-	sns.listSubscriptionsByTopic({
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
-	}).promise()
-		.then(data => {
-			console.log('success listSubscriptionsByTopic ', data);
-		})
-		.catch(err => {
-			console.log('success listSubscriptionsByTopic ', data);
+			console.log('error confirmSubscription ',err)
 		});
 
-	sns.setTopicAttributes({
-		AttributeName: 'DisplayName',
-		AttributeValue: 'Changed by the lambda',
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
-	}).promise()
-		.then(data => {
-			console.log('success setTopicAttributes ', data);
-		})
-		.catch(err => {
-			console.log('success setTopicAttributes ', data);
-		});
-
-	sns.subscribe({
-		Protocol: 'email',
-		Endpoint: 'hirudineel@gmail.com',
-		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
-	}).promise()
-		.then(data => {
-			console.log('success subscribe ', data);
-		})
-		.catch(err => {
-			console.log('success subscribe ', data);
-		});
 
 
 
