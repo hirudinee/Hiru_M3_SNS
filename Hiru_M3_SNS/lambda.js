875ffc92bf0967bd61b5b1ddc4dcef3c787fda86
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 11:50:24 +0530

    updated commit - add a new resource

--- Hiru_M3_SNS/lambda.js
+++ Hiru_M3_SNS/lambda.js
@@ -1,6 +1,39 @@
 let AWS = require('aws-sdk');
-exports.handler = function(event, context, callback) {
+const sns = new AWS.SNS();
+exports.handler = function (event, context, callback) {
 
-	console.log('This is the log to know that trigger has startted',event)
-	callback(null,'Successfully executed');
+	console.log('This is the log to know that trigger has started', event)
+	sns.publish({
+		Message: 'this is a test message',
+		Subject: 'test',
+		MessageAttributes: {
+			'key001': {
+				DataType: 'String',
+				StringValue: 'test'
+			},
+			'key003': {
+				DataType: 'Number',
+				StringValue: '001'
+			},
+			'key004': {
+				DataType: 'String.Array',
+				StringValue: '11'
+			},
+			'key002': {
+				DataType: 'Binary',
+				StringValue: '1'
+			},
+		},
+		MessageStructure: 'String',
+		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
+	}).promise()
+		.then(data => {
+			console.log('success ',data)
+		})
+		.catch(err => {
+			console.log('err ',err)
+		});
+
+	
+	callback(null, 'Successfully executed');
 }
