174735409585b2f08342dbc7455f2fe622473fee
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-29 13:28:14 +0530

    updated commit - get attribute resource

--- Hiru_M3_SNS/lambda.js
+++ Hiru_M3_SNS/lambda.js
@@ -2,33 +2,17 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
-	console.log('This is the log to know that trigger has started', event)
-	sns.publish({
-		Message: 'this is a test message',
-		Subject: 'test',
-		MessageAttributes: {
-			'key001': {
-				DataType: 'String',
-				StringValue: 'test'
-			},
-			'key003': {
-				DataType: 'Number',
-				StringValue: '001'
-			},
-			'key004': {
-				DataType: 'String.Array',
-				StringValue: '11'
-			}
-		},
-		MessageStructure: 'String',
+	console.log('This is the log to know that trigger has started', event);
+	sns.getTopicAttributes({
 		TopicArn: 'arn:aws:sns:us-east-1:480964559519:new_resource_hiru'
 	}).promise()
 		.then(data => {
-			console.log('success ', data)
+			console.log('success ',data);
 		})
 		.catch(err => {
-			console.log('err ', err)
+			console.log('error ',err);
 		});
+
 
 
 	callback(null, 'Successfully executed');
